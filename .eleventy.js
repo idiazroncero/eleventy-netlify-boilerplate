2709f8b1a4d8f34e41ec12fdd8292ee956fb7278
Nacho Diaz <idiaz.roncero@gmail.com>
2018-09-20 13:50:21 +0200

    Put Huesos on root

--- .eleventy.js
+++ .eleventy.js
@@ -55,6 +55,7 @@ module.exports = function(eleventyConfig) {
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("huesos/dist");
 
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
