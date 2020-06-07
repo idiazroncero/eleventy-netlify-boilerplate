437f1c4453009650a791ed9cd98b9ef9ff05ff5d
Nacho Diaz <idiaz,roncero@gmail.com>
2020-06-07 18:08:10 +0200

    Migrate to typescript

--- src/assets/js/modules/module.js
+++ src/assets/js/modules/module.js
@@ -1,3 +1,6 @@
-export default function(string){
+"use strict";
+exports.__esModule = true;
+function default_1(string) {
     console.log(string);
 }
+exports["default"] = default_1;
---
+++ src/assets/js/script.ts
@@ -0,0 +1,8 @@
+import testModule from './modules/module';
+
+testModule('Hola Mundo! Esto es un mensaje logueado desde un módulo escrito en Typescript importado usando Parcel.js');
+
+var casa = function(hola) {
+    var hola = hola;
+    return hola;
+}
